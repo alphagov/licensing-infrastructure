89503ac38b967e32da0ee019e71d8e921295ae3c
Alice Lee <alice.lee2@digital.cabinet-office.gov.uk>
2026-09-09 14:49:47 +0100

    Add terraform state s3 bucket to bootstrap

---
+++ bootstrap/s3.tf
@@ -0,0 +1,39 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "govuk-licensing-${var.environment}-tfstate"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public_terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
