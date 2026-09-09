63dfbd353c8e397115ba83e3a428687c21c8e2c3
Alice Lee <alice.lee2@digital.cabinet-office.gov.uk>
2026-09-09 16:34:23 +0100

    Add power user engineer role to bootstrap

--- bootstrap/iam.tf
+++ bootstrap/iam.tf
@@ -34,6 +34,51 @@ resource "aws_iam_role_policy_attachment" "allow_admin" {
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
+resource "aws_iam_role" "power_user" {
+  name               = "PowerUser"
+  assume_role_policy = jsonencode(local.engineer_assume_role_policy)
+}
+
+resource "aws_iam_policy" "power_user_access" {
+  name        = "PowerUserAccess"
+  path        = "/"
+  description = "Access to everything except most account and permissions actions"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        NotAction = [
+          "iam:*",
+          "organizations:*",
+          "account:*"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "account:GetAccountInformation",
+          "account:GetPrimaryEmail",
+          "account:ListRegions",
+          "iam:CreateServiceLinkedRole",
+          "iam:DeleteServiceLinkedRole",
+          "iam:ListRoles",
+          "organizations:DescribeEffectivePolicy",
+          "organizations:DescribeOrganization"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "power_user_access" {
+  role       = aws_iam_role.power_user.name
+  policy_arn = aws_iam_policy.power_user_access.arn
+}
+
 resource "aws_iam_role" "read_only" {
   name               = "ReadOnly"
   assume_role_policy = jsonencode(local.engineer_assume_role_policy)
