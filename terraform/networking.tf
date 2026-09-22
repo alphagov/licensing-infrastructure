8f5f7e48db7e506f9b713c5393a7f316a73e4e81
Alice Lee <alice.lee2@digital.cabinet-office.gov.uk>
2026-09-22 16:26:56 +0100

    Use a shared regional NAT gateway instead of multiple zonal gateways

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -4,10 +4,26 @@ resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
+
+  tags = {
+    Name = "main-${var.environment}"
+  }
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
+}
+
+resource "aws_eip" "egress" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "egress" {
+  vpc_id            = aws_vpc.main.id
+  availability_mode = "regional"
+  connectivity_type = "public"
+
+  depends_on = [aws_internet_gateway.igw]
 }
 
 ### Public subnets
@@ -49,31 +65,16 @@ resource "aws_subnet" "private" {
 }
 
 resource "aws_route_table" "private" {
-  for_each = aws_subnet.private
-  vpc_id   = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.egress[each.key].id
+    nat_gateway_id = aws_nat_gateway.egress.id
   }
 }
 
 resource "aws_route_table_association" "private" {
   for_each       = aws_subnet.private
   subnet_id      = each.value.id
-  route_table_id = aws_route_table.private[each.key].id
-}
-
-### NAT gateways
-
-resource "aws_eip" "egress" {
-  for_each = toset(var.availability_zones)
-  domain   = "vpc"
-}
-
-resource "aws_nat_gateway" "egress" {
-  connectivity_type = "public"
-  vpc_id            = aws_vpc.main.id
-
-  depends_on = [aws_internet_gateway.igw]
+  route_table_id = aws_route_table.private.id
 }
