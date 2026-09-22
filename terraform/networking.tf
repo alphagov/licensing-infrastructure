9738d44ddf0c5bdf31cef23ebae8c9ff08877ce7
Alice Lee <alice.lee2@digital.cabinet-office.gov.uk>
2026-09-22 16:38:16 +0100

    Add names to networking resources
    Makes it much easier to work out what's going on in the web console

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -12,18 +12,30 @@ resource "aws_vpc" "main" {
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "igw-${var.environment}"
+  }
 }
 
-resource "aws_eip" "egress" {
+resource "aws_eip" "nat_gw" {
   domain = "vpc"
+
+  tags = {
+    Name = "nat-gw-${var.environment}"
+  }
 }
 
-resource "aws_nat_gateway" "egress" {
+resource "aws_nat_gateway" "nat_gw" {
   vpc_id            = aws_vpc.main.id
   availability_mode = "regional"
   connectivity_type = "public"
 
   depends_on = [aws_internet_gateway.igw]
+
+  tags = {
+    Name = "nat-gw-${var.environment}"
+  }
 }
 
 ### Public subnets
@@ -36,6 +48,10 @@ resource "aws_subnet" "public" {
   cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, index(var.availability_zones, each.value))
   availability_zone       = each.value
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-${var.environment}-${each.value}"
+  }
 }
 
 resource "aws_route_table" "public" {
@@ -44,6 +60,10 @@ resource "aws_route_table" "public" {
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-${var.environment}"
   }
 }
 
@@ -62,6 +82,10 @@ resource "aws_subnet" "private" {
   # Public subnets are 10.0.10.0/24, 10.0.11.0/24 etc.
   cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, index(var.availability_zones, each.value) + 10)
   availability_zone = each.value
+
+  tags = {
+    Name = "private-${var.environment}-${each.value}"
+  }
 }
 
 resource "aws_route_table" "private" {
@@ -69,7 +93,11 @@ resource "aws_route_table" "private" {
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.egress.id
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+
+  tags = {
+    Name = "private-${var.environment}"
   }
 }
 
